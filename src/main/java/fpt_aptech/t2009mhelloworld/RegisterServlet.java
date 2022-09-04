f1f035665120d2dd62c121cd42b30c2990e7de94
ADMIN <ADMIN@DESK^Cgit config --global user.email ADMIN@DESK^C>
2022-09-04 19:09:33 +0700

    Initial commit

---
+++ src/main/java/fpt_aptech/t2009mhelloworld/RegisterServlet.java
@@ -0,0 +1,20 @@
+package fpt_aptech.t2009mhelloworld;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RegisterServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/user/Register.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
